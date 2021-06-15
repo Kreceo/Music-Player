69ec3cb7020bf374d3da6f12f7fdd0c24f27e8c2
Kreceo <darrell-lane@hotmail.co.uk>
2021-06-15 09:27:13 +0100

    Feature ( Adding state components for buttons )

--- components/Controls.js
+++ components/Controls.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import { ButtonWrapper } from './Buttons';
 
@@ -8,14 +9,18 @@ const ControlWrapperClass = styled.div`
     width: 100%; 
 `
 
-export function ControlsWrapper() {
+export function ControlsWrapper () {
+    const [playing, setPlaying] = useState(false);
+    const [count, setCount] = useState(0);
+
     return (
         <ControlWrapperClass>
-            <ButtonWrapper type="fas fa-step-backward" size="Small" title="Back"/> 
-            <ButtonWrapper type="fas fa-backward" size="Medium" title="Rewind"/> 
-            <ButtonWrapper type="fas fa-play" size="Large" title="Play"/> 
-            <ButtonWrapper type="fas fa-forward" size="Medium" title="Forward"/> 
-            <ButtonWrapper type="fas fa-step-forward" size="Small" title="Skip"/> 
+            <ButtonWrapper clickControl={() => setCount(count + 1)} type="fas fa-step-backward" size="Small" title="Previous"/> 
+            <p>{count}</p>
+            <ButtonWrapper clickControl={() => setCount(count + 1)} type="fas fa-backward" size="Medium" title="Start"/> 
+            <ButtonWrapper clickControl={() => setPlaying(playing == false) ? true : false} type="fas fa-play" size="Large" title={playing == false ? 'Play' : 'Paused'}/> 
+            <ButtonWrapper clickControl={() => setCount(count + 1)} type="fas fa-forward" size="Medium" title="End"/> 
+            <ButtonWrapper clickControl={() => setCount(count + 1)} type="fas fa-step-forward" size="Small" title="Next"/> 
         </ControlWrapperClass>
     )
 }
